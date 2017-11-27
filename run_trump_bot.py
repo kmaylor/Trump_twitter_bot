27caee477d0119e39d43d91b799077acd6599565
kmaylor <kmaylor@ucdavis.edu>
2017-11-27 08:53:41 -0800

    fixed time calc bug

--- run_trump_bot.py
+++ run_trump_bot.py
@@ -5,106 +5,18 @@ import re
 import json
 from collections import defaultdict
 import random
-from numpy import arange, where, isin, mean, array, exp, sqrt, dot, pi, diag, concatenate
+from numpy import arange, where, isin, mean, array, exp, sqrt, dot, pi, diag, concatenate, histogram
 from bs4 import BeautifulSoup
 import requests
 from scipy.optimize import curve_fit
 from random import uniform
 from numpy.random import normal
-from matplotlib.pyplot import hist, show, plot, xlim, legend
-from datetime.datetime import now
-
-#load twitter keys
-twitter_keys=p.load(open('tk.pkl','rb'))
-
-auth = OAuthHandler(twitter_keys['consumer_key'], twitter_keys['consumer_secret'])
-auth.set_access_token(twitter_keys['access_token'], twitter_keys['access_secret'])
- 
-api = tweepy.API(auth)
-
-#grab the time
-today = str(now())[:10]
-
-del twitter_keys, auth
-while True:
-    
-    #At midnight start a new day of tweeting
-	if today!=str(now())[:10]:
-        
-        #Grab Trumps tweets and store them in a json dictionary
-		hold=[]
-        with open('test.txt','w') as f:
-            json.dump([], f)
-        for i,status in enumerate(limit_handled(tweepy.Cursor(api.user_timeline,'realDonaldTrump').items(10000))):
-            hold.append(status._json)
-            if i%100==0:
-                with open('test.txt','r') as f:
-                    a=json.load(f)+hold
-                with open('test.txt','w') as f:
-                    json.dump(a, f)
-                hold=[]
-
-        trump_tweets=json.load(open('test.txt','r'))
-        
-        #count the number of times Trump tweets each day 
-        dt = tweets_per_day(trump_tweets)
-        
-        #fit a guassian to the number of times trump tweets each day
-        params = fit_pdf([i for v in dt.values() for i in v],100,gauss,(10,4))
-        
-        #draw from distribution to determine the number of times to tweet (num_of_tweets), minimum of once per day.
-        num_of_tweets=0
-        while num_of_tweets <=0:
-            num_of_tweets=int(round(normal(params[0],params[1])))
-        num_of_tweets
-        
-        # determine when Trump usually tweets. It appears to be a bimodal distribution if we look at a 24 hour
-        # cycle and start at 7am
-        times=tweet_times(trump_tweets)-7 
-        for i,t in enumerate(times):
-            if t<0:
-                times[i]+=24
-       
-        #fit the tweet time distribution as a bimodal 
-        params = fit_pdf(times,arange(0,24,.25),bimodal,(5,1,.1,15,5))
-        
-        #draw from the tweet time distribution, bot will tweet at each value of times
-        times=[]
-        for i in arange(num_of_tweets):
-            time=25
-            while (time<0 or time>24):
-                if uniform(0,1) >= params[2]:
-                    time = normal(params[3],params[4])
-                else:
-                    time = normal(params[0],params[1])
-                time+=7
-                if time>24: time-=24
-            times.append(time)
-            print(times)
-
-        # free memory, better to reload than leave it since this will run continuously
-        del trump_tweets, dt, params, num_of_tweets, time, hold, a, i, status
-
-        for t in sorted(times):
-        	if t >= convert_time(str(datetime.now())[11:19]):
-                gen_tweet=['.','.','.','.','.']
-                while gen_tweet.count('.')>4:
-                    try:
-                        gen_tweet=nxm_grams(3,1,tweets_to_list(json.load(open('test.txt','r'))))
-                    except IndexError:
-                        gen_tweet[:3]==['.','.','.','.','.']
-                print(gen_tweet)
-                #api.update_status(gen_tweet)
-            else:
-            	time.sleep((convert_time(str(datetime.now())[11:19])-t)*60**2)
-        today = str(now())[:10]
-
-    else:
-    	time.sleep((24-(convert_time(str(datetime.now())[11:19])))*60**2)
-
+from datetime import datetime
+import time
+from pytz import timezone
 
 def fit_pdf(data,bins,pdf,expected):
-	counts,bounds,_ = hist(data,bins=bins,normed=True)
+    counts,bounds = histogram(data,bins=bins,normed=True)
     bin_c=(bounds[1:]+bounds[:-1])/2
     params,_ =curve_fit(pdf,bin_c,counts,expected)
     return params
@@ -217,3 +129,96 @@ def nxm_grams(n,m,words):
             current =  current[m+1-n:]+(*next,)
         for c in current[-m:]: result.append(c)
         if current[-1] in ['.','!','?'] and len(result)>5: return ' '.join(result)
+
+#load twitter keys
+twitter_keys=p.load(open('tk.pkl','rb'))
+
+auth = OAuthHandler(twitter_keys['consumer_key'], twitter_keys['consumer_secret'])
+auth.set_access_token(twitter_keys['access_token'], twitter_keys['access_secret'])
+ 
+api = tweepy.API(auth)
+
+#grab the time
+today = str(datetime.now(timezone('US/Eastern')))[:10]
+
+del twitter_keys, auth
+while True:
+    
+    #At midnight start a new day of tweeting
+    if today!=str(datetime.now(timezone('US/Eastern')))[:10]:
+        
+        #Grab Trumps tweets and store them in a json dictionary
+        hold=[]
+        with open('test.txt','w') as f:
+            json.dump([], f)
+        for i,status in enumerate(limit_handled(tweepy.Cursor(api.user_timeline,'realDonaldTrump').items(10000))):
+            hold.append(status._json)
+            if i%100==0:
+                with open('test.txt','r') as f:
+                    a=json.load(f)+hold
+                with open('test.txt','w') as f:
+                    json.dump(a, f)
+                    hold=[]
+
+        trump_tweets=json.load(open('test.txt','r'))
+        
+        #count the number of times Trump tweets each day 
+        dt = tweets_per_day(trump_tweets)
+        
+        #fit a guassian to the number of times trump tweets each day
+        dt_params = fit_pdf([i for v in dt.values() for i in v],100,gauss,(10,4))
+        
+        #draw from distribution to determine the number of times to tweet (num_of_tweets), minimum of once per day.
+        num_of_tweets=0
+        while num_of_tweets <=0:
+            num_of_tweets=int(round(normal(dt_params[0],dt_params[1])))
+        num_of_tweets
+        
+        # determine when Trump usually tweets. It appears to be a bimodal distribution if we look at a 24 hour
+        # cycle and start at 7am
+        times=tweet_times(trump_tweets)-7 
+        for i,t in enumerate(times):
+            if t<0:
+                times[i]+=24
+       
+        #fit the tweet time distribution as a bimodal 
+        time_params = fit_pdf(times,arange(0,24.25,.25),bimodal,(5,1,.1,15,5))
+        
+        #draw from the tweet time distribution, bot will tweet at each value of times
+        times=[]
+        for i in arange(num_of_tweets):
+            t=25
+            while (t<0 or t>24):
+                if uniform(0,1) >= time_params[2]:
+                    t = normal(time_params[3],time_params[4])
+                else:
+                    t = normal(time_params[0],time_params[1])
+                t+=7
+                if t>24: t-=24
+            times.append(t)
+        print(times)
+
+        # free memory, better to reload than leave it since this will run continuously
+        del trump_tweets, dt, dt_params, time_params, num_of_tweets, hold, a, i, status
+
+        for t in sorted(times):
+            while True:
+                if (t > convert_time(str(datetime.now(timezone('US/Eastern')))[11:19])-1/60) and \
+                    (t < convert_time(str(datetime.now(timezone('US/Eastern')))[11:19])+1/60):
+                    gen_tweet=['.','.','.','.','.']
+                    while gen_tweet.count('.')>4:
+                        try:
+                            gen_tweet=nxm_grams(3,1,tweets_to_list(json.load(open('test.txt','r'))))
+                        except IndexError:
+                            gen_tweet==['.','.','.','.','.']
+                    print(gen_tweet,t,convert_time(str(datetime.now(timezone('US/Eastern')))[11:19]))
+                    #api.update_status(gen_tweet)
+                    break
+                else:
+                    time.sleep((convert_time(str(datetime.now(timezone('US/Eastern')))[11:19])-t)*60**2)
+        today = str(datetime.now(timezone('US/Eastern')))[:10]
+
+    else:
+        time.sleep((24-(convert_time(str(datetime.now(timezone('US/Eastern')))[11:19])))*60**2)
+
+
